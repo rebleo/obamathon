be04e626db6eb139241347d3742deabdee76fe1b
rebleo <rml444@nyu.edu>
2017-01-09 16:43:50 -0500

    obama yrFace

--- obamaYrFace/js/sketch.js
+++ obamaYrFace/js/sketch.js
@@ -1,24 +1,24 @@
-//     ( POTUS yrFace )
+//     Obama Yr Face
 // rebecca (marks) leopold, 2017
-////also thx zac, kevin, jasmine, aaron for letting me "share" yr idea.
-//http://www.potatoyourface.com/
-//using clmtrackr - https//github.com/auduno/clmtrackr
+// http://rml444.itp.io/obamaYrFace
+// inspired by: http://www.potatoyourface.com/
+// thx Zac Coble, Kevin Steirnweis, Jasmine Soltani & Aarón Moraga  Montoya
+// using clmtrackr - https//github.com/auduno/clmtrackr
 
 var theCanvas;
 var theContxt;
 var theTitleCanvas;
-var theTitleText;
+var theTitleTxt;
 
 var meFace;
-var feelings = ["angry", "sad", "suprised", "happy"]
 
 var ec = new emotionClassifier();
 ec.init(emotionModel);
 var emotionData = ec.getBlank();
 
 var emojiGo = function() {
-
-/////// communication / compatibility jig /////
+//Get User Media accesses the user's webcam
+//https://itp.nyu.edu/~sve204/liveweb_fall2016/week4.html
 	window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
@@ -30,7 +30,7 @@ var emojiGo = function() {
 		navigator.getUserMedia({video: true}, function(stream) {
 				meFace.src = window.URL.createObjectURL(stream) || stream;
 				meFace.play();
-				//call draw after get video info
+				//call draw - all the Canvas events, after the video has loaded so the data has a place to go.
 				draw();
 
 			}, function(error) {alert("Failure " + error.code);});
@@ -40,13 +40,13 @@ var emojiGo = function() {
 	theCanvas = document.getElementById('thecanvas1');
  	theContxt = theCanvas.getContext('2d');
 	theTitleCanvas = document.getElementById('thecanvas2');
-	theTitleText = theTitleCanvas.getContext('2d');
+	theTitleTxt = theTitleCanvas.getContext('2d');
 
-	theTitleText.fillStyle = "green";
-	theTitleText.fillRect(0, 0, theTitleCanvas.width, theTitleCanvas.height);
-	// theTitleText.font = "20px Georgia";
-	// theTitleText.fillStyle = "blue";
-	// theTitleText.fillText("Obama Looking...",50,40);
+	theContxt.fillStyle = "green";
+	theContxt.fillRect(0, 0, theCanvas.width, theCanvas.height);
+	theTitleTxt.fillStyle = "green";
+	theTitleTxt.fillRect(0, 0, theTitleCanvas.width, theTitleCanvas.height);
+
 
 //init the CLM tracker
 	ctracker = new clm.tracker();
@@ -57,20 +57,13 @@ var emojiGo = function() {
 
 
 var draw = function() {
-//ANYTHING DRAWN ON CANVAS WILL BE EMITTED YOU FOOL!
+//draw the image to the canvas
  	theContxt.drawImage(meFace,0,0,meFace.width,meFace.height);
 	var title = document.getElementById('obama');
-	theTitleText.drawImage(title, 35,100);
-	var happy = document.getElementById('word4');
-	theTitleText.drawImage(happy, 100, 200);
-	var sad = document.getElementById('word2');
-	theTitleText.drawImage(sad, 250,  200);
-	var suprised = document.getElementById('word3');
-	theTitleText.drawImage(suprised, 400,  200);
-	var mad = document.getElementById('word1');
-	theTitleText.drawImage(mad, 550,  200);
 
-	theContxt.drawImage(title, 0, 410)
+	theContxt.drawImage(title, 0, 425)
+	var emo = document.getElementById('emo');
+	theTitleTxt.drawImage(emo, 0,0);
 //all this clm tracker emotion stuff
 	var cp = ctracker.getCurrentParameters();
 	 //angry, sad, suprised, happy
@@ -87,10 +80,7 @@ var draw = function() {
 					if (expression[j].value > 0.4) {
 						var myEmo = document.getElementById('icon'+(j+1));
 						// console.log(j);
-						// var myFeeling = feelings[j];
-						// console.log(myFeeling)
 						theContxt.drawImage(myEmo,positions[i][0]-50, positions[i][1]-200);
-						// var feelingTitle = theTitleText.fillText(myFeeling, 225, 40 + (j * 40));
 
 					} else {
 						// feelingTitle = theTitleText.fillText(" ",225,40);
@@ -102,8 +92,10 @@ var draw = function() {
 
 		}
 	 }
-///whatever the canvas state is at this point will be sent over the network
+	//turn the video data into image data
  	var meData = theCanvas.toDataURL('image/jpeg', 0.2);
+	//draw those images ever 1/10 of second so appears "live"
  	setTimeout(draw,10);
 }
+//when the page loads, call the program to run
 window.addEventListener('load', emojiGo);
