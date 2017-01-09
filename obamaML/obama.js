b895bcd9a77b8ba600848d2f3d68260c480e1f61
rebleo <rml444@nyu.edu>
2017-01-08 23:25:34 -0500

    privacy plz

--- obamaML/obama.js
+++ obamaML/obama.js
@@ -5,8 +5,8 @@ var theWords = [];
 
 // instantiate a new Clarifai app passing in your clientId and clientSecret
 var app = new Clarifai.App(
-	'Sb8yECwZ6h3c00Oh7bAOzCESq-MVxapDidkcssEc',
-	'yMtPNkdWeZtKIH2suMv0893R4RA4BeB2dYbB8WXn'
+	'',
+	''
 );
 
 		// predict the contents of an image by passing in a url
